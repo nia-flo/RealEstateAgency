ad39fbbc44cce38c2b4397b3e1d2ec0f5763ebb3
Simeon Yordanov <76979714+PottedPlantt@users.noreply.github.com>
2022-05-06 07:11:02 +0300

    Insert data

---
+++ insert data/deal_company.sql
@@ -0,0 +1,257 @@
+INSERT INTO Deal_BuyerCompany
+  VALUES(1,'9683484')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(2,'3808806')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(3,'8012509')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(4,'3808806')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(5,'7838371')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(6,'6539246')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(7,'1503649')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(8,'7838371')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(9,'1503649')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(10,'9307787')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(11,'9307787')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(12,'0165754')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(13,'7777335')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(14,'6539246')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(15,'8907815')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(16,'9683484')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(17,'6509006')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(18,'5328253')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(19,'5200391')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(20,'7838371')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(21,'4076361')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(22,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(23,'6772294')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(24,'5200391')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(25,'1503649')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(26,'6509006')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(27,'6184963')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(28,'8907815')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(29,'9307787')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(30,'4076361')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(31,'3808806')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(32,'3808806')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(33,'5766582')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(34,'7777335')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(35,'6509006')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(36,'8012509')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(37,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(38,'9683484')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(39,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(40,'8907815')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(41,'9307787')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(42,'8907815')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(43,'6772294')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(44,'4076361')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(45,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(46,'7838371')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(47,'5200391')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(48,'9307787')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(49,'6184963')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(50,'7777335')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(51,'5200391')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(52,'4076361')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(53,'1250839')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(54,'6184963')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(55,'6184963')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(56,'1250839')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(57,'5766582')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(58,'4076361')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(59,'6772294')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(60,'1503649')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(61,'0165754')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(62,'9683484')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(63,'5766582')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(64,'6539246')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(65,'7777335')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(66,'0165754')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(67,'8907815')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(68,'6772294')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(69,'6772294')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(70,'1250839')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(71,'7838371')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(72,'1503649')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(73,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(74,'6509006')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(75,'3808806')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(76,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(77,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(78,'4288758')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(79,'5766582')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(80,'5766582')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(81,'9683484')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(82,'1503649')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(83,'9307787')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(84,'0165754')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(85,'8012509')
+
+INSERT INTO Deal_BuyerCompany
+  VALUES(86,'6772294')
