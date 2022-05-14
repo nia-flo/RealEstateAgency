40918f391b552a1e9f5cf1fc165cdb25db82a010
NanoAnubis <lubomir_stoykov@abv.bg>
2022-05-14 14:29:09 +0300

    Created Triggers.sql and one trigger in it

---
+++ Triggers.sql
@@ -0,0 +1,18 @@
+USE RealEstateAgency;
+
+---On deletion in Employee, column has_quit changes to yes and prints all deals done by this agent
+GO
+CREATE TRIGGER delete_employee ON Employee 
+INSTEAD OF DELETE
+AS
+UPDATE Employee
+SET has_quit = 'yes' --- default value for column has_quit is 'no' or NULL
+WHERE EGN IN (SELECT EGN FROM DELETED)
+BEGIN
+	SELECT id,notary,estate,date,price,conditions,realEstateAgent,commissionPersentage 
+	FROM Deal
+	JOIN Employee
+	ON realEstateAgent=EGN
+	WHERE EGN=(SELECT EGN FROM DELETED)
+END
+GO
