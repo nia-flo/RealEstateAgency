57662154a91a5f942a4e92f6e34724a79627c5c9
RayaSimeonova <72245230+RayaSimeonova@users.noreply.github.com>
2022-05-18 14:01:20 +0300

    Added semicolons at end of queries and fixed tab size to 4

--- Joins.sql
+++ Joins.sql
@@ -3,7 +3,7 @@ SELECT e.id, e.city, e.address
 FROM Estate_Owner eo
 RIGHT JOIN Estate e ON eo.estate = e.id
 WHERE eo.owner IS NULL
-	AND e.type = 'rent'
+	AND e.type = 'rent';
 
 
 -- 2) Names of companies that bought an estate for a lower price than the declared
@@ -13,7 +13,7 @@ JOIN Deal_BuyerCompany bc ON bc.deal = d.id
 JOIN Company c ON c.bulstat = bc.buyer
 JOIN Estate e ON d.estate = e.id
 WHERE d.price < e.price
-	AND e.type = 'sale'
+	AND e.type = 'sale';
 
 
 -- 3) Names of female owners in alphabetical order whose estates haven't been rented or sold 
@@ -23,7 +23,7 @@ JOIN Person p ON p.EGN = eo.owner
 LEFT JOIN Deal d ON eo.estate = d.estate
 WHERE d.id IS NULL
 	AND p.gender = 'f'	
-ORDER BY p.name
+ORDER BY p.name;
 	
 
 -- 4) Name of the notary from the last deal with a buyer company
@@ -33,7 +33,7 @@ JOIN Person p ON p.EGN = d.notary
 JOIN Deal_BuyerCompany bc ON d.id = bc.deal
 WHERE d.date >= ALL ( SELECT d2.date
 								FROM Deal d2
-								JOIN Deal_BuyerCompany bc2 ON bc2.deal = d2.id)
+								JOIN Deal_BuyerCompany bc2 ON bc2.deal = d2.id);
 
 
 -- 5) Names of companies that haven't bought or rented an estate between 01/01/2015 and 01/01/2019
@@ -44,5 +44,5 @@ SELECT c2.name
 FROM Deal d
 JOIN Deal_BuyerCompany bc ON bc.deal = d.id
 JOIN Company c2 ON c2.bulstat = bc.buyer
-WHERE d.date BETWEEN '2015-01-01' AND '2019-01-01'
+WHERE d.date BETWEEN '2015-01-01' AND '2019-01-01';
 
